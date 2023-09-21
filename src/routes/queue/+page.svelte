83e901b960c4a72bf7424f0151bb34ca9d896be9
ArianeNicolas <98948015+ArianeNicolas@users.noreply.github.com>
2023-09-21 11:04:44 +0200

    page temps de queue

---
+++ src/routes/queue/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    import TempsQueue from './TempsQueue.svelte'
+</script>
+
+<TempsQueue nomRU="Astrolabe", nombreDansQueue=[10,50,100,200,800]></TempsQueue>
---
+++ src/routes/queue/TempsQueue.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { Card, CardBody, CardHeader, CardTitle } from 'yesvelte';
+    import { Progress, ProgressBar } from 'yesvelte';
+    import { Slider, SliderKnob } from "yesvelte";
+    export var nomRU;
+    export let nombreDansQueue;
+    export var time;
+    let list = {};
+
+    {#each items as nombreDansQueue}
+	    if(items.nombre)
+    {/each}
+
+</script>
+
+<Card title={nomRU}>
+	<CardBody>Temps de queue
+        <Progress>
+            <ProgressBar value="20" color="green" />
+            <ProgressBar value="50" color="warning" />
+            <ProgressBar value="70" color="danger" />
+            <ProgressBar value="80" color="warning" />
+            <ProgressBar value="100" color="green" />
+        </Progress>
+        <Slider>
+            <SliderKnob disabled bind:time />
+        </Slider>
+    </CardBody>
+</Card>
