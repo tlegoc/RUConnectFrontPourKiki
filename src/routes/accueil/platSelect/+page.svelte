2c31e1019e966280c13b62e59e1d291153f7ecc4
Rémi Cazoulat <remi.cazoulat35@gmail.com>
2023-09-21 10:22:05 +0200

    blzbla

--- src/routes/accueil/platSelect/+page.svelte
+++ src/routes/accueil/platSelect/+page.svelte
@@ -11,18 +11,7 @@
 </script>
 
 
-<El colLg="8" colMd="10" offsetLg="2" offsetMd="1">
-	<Card>
-		<CardMedia elementPosition="top">
-			<img
-				src="https://preview.tabler.io/static/photos/home-office-desk-with-macbook-iphone-calendar-watch-and-organizer.jpg"
-				alt="" />
-		</CardMedia>
-		<CardBody>
-			<!--  -->
-		</CardBody>
-	</Card>
-</El>
+
 <Card>
 	<Button color="primary" size="lg">Ajouter Plat</Button>
 	<Button color="secondary" size="lg">Changer ru</Button>
