83e2f2ea1cc860416977288202eb76a484036a5f
lordsmourbif35 <axel.allain35@gmail.com>
2023-09-21 10:22:03 +0200

    First pages to add food

---
+++ src/routes/add_meal/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { Button, Icon, El} from 'yesvelte'
+	export let stand_number1 = 1;
+	export let stand_number2 = 2;
+	export let stand_number3 = 3;
+
+	function goTo() {
+		goto('/add_meal/add_ingredients');
+	}
+
+</script>
+
+<El container>
+	<El row rowCols="1">
+		<El col>
+			<Button color="azure" on:click={goTo}>
+				Stand {stand_number1}
+			</Button>
+		</El>
+		<El col>
+			<Button color="secondary">
+				<Icon name="plus" />Ajouter un plat
+			</Button>
+		</El>
+	</El>
+</El>
---
+++ src/routes/add_meal/add_ingredients/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { Button, Icon, El, Offcanvas, OffcanvasBody, OffcanvasHeader, Input  } from 'yesvelte'
+    let show = false
+</script>
+
+<El container>
+	<El row rowCols="1">
+		<El col>
+			<Button color="green">
+				Hamburger
+			</Button>
+		</El>
+        <El col>
+			<Button color="green">
+				Frites
+			</Button>
+		</El>
+		<El col>
+			<Button color="secondary" on:click={() => (show = !show)}>
+				<Icon name="plus" />Ajouter un ingrédient
+			</Button>
+		</El>
+	</El>
+</El>
+
+<Offcanvas bind:show>   
+	<OffcanvasHeader title="Ajouter un ingrédient" />
+	<OffcanvasBody>
+        <Input placeholder="Cherchez un ingrédient">
+            <Icon slot="food" name="pajamas:food" />
+        </Input>
+	</OffcanvasBody>
+</Offcanvas>
