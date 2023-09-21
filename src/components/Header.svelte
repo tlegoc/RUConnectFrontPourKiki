8d8893829562cc4d405dcd9cf20534c1508ce0fd
kilian.marcelin@gmail.com <kilian.marcelin@askoed.com>
2023-09-21 09:10:27 +0200

    cado

---
+++ src/components/Header.svelte
@@ -0,0 +1,16 @@
+<header>
+    <h1 class="title">RU CONNECT</h1>
+</header>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;400&family=Roboto:wght@100&display=swap');
+
+    header{
+        background: #f7f7f7;
+    }
+    h1{
+        font-family: 'Poppins', sans-serif;
+        text-align: center;
+        font-size: large;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,24 @@
 <script>
-	import tabler from 'yesvelte/css/tabler.min.css?url'
+	import Header from '../components/Header.svelte'
 </script>
+
 <svelte:head>
-	<link rel='stylesheet' href={tabler}/>
+	<Header/>
+	<h1 class="title">RU CONNECT</h1>
 </svelte:head>
+
 <slot/>
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;400&family=Roboto:wght@100&display=swap');
+
+	:global(*){
+		font-family: 'Poppins', sans-serif;
+	}
+	:global(.center){
+		text-align: center;
+	}
+	:global(.title){
+		text-align: center;
+        font-size: xx-large;
+	}
+</style>
