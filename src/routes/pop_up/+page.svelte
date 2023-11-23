3613850d54d022d9763f4b5989ce3c150a762269
lordsmourbif35 <axel.allain35@gmail.com>
2023-11-23 08:20:06 +0100

    Create +page.svelte

---
+++ src/routes/pop_up/+page.svelte
@@ -0,0 +1,29 @@
+
+<script>
+  let showPopup = true;
+
+  function togglePopup() {
+    showPopup = !showPopup;
+  }
+</script>
+
+{#if showPopup}
+  <div class="popup">
+    <p>Are you in a queue for the university restaurant?</p>
+    <button on:click={togglePopup}>No</button>
+    <button on:click={togglePopup}>Yes</button>
+  </div>
+{/if}
+
+<style>
+  .popup {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: white;
+    padding: 20px;
+    border: 1px solid black;
+    z-index: 9999;
+  }
+</style>
