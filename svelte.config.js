69c655caa33c7559ab1879d11a5c94a0c1effd6e
kmarcelin <kilian.marcelin@etudiant.univ-rennes1.fr>
2023-12-14 09:25:06 +0100

    switch to adapter auto

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 
-		adapter:  adapter()
+		adapter:  adapter(),
 	}
 };
 
