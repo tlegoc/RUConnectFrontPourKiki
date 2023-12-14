62c3b12b34965a01467c3501d9226f031637a1c0
ArianeNicolas <ariane.nicolas.bis@gmail.com>
2023-12-14 09:17:39 +0100

    tinder

---
+++ src/routes/tinderbeau/+page.svelte
@@ -0,0 +1,142 @@
+<script defer>
+
+	import { Button } from 'yesvelte';
+    import { Icon } from 'yesvelte';
+    let plats = ["fromage","paella","ratatouille","galette de sarrazin","tomate"];
+    let images = ["/src/images/fromage.png","/src/images/paella.jpg","/src/images/ratatouille.jpg","/src/images/galette.jpg","/src/images/tomate.webp"]
+    let bool = [];
+    let length = plats.length;
+    let width;
+    let i = 0;
+    function like(){
+        if(i < length){
+            bool[i] = true;
+        i++;
+        }
+        
+    }
+    function dislike(){
+        if(i < length){
+            bool[i] = false;
+        i++;
+        }
+        
+    }
+
+    let currentIndex = 0;
+    let startX = 0;
+    let isDragging = false;
+
+    function handleStart(event) {
+        startX = event.touches ? event.touches[0].clientX : event.clientX;
+        isDragging = true;
+    }
+
+    function handleMove(event) {
+        if (isDragging) {
+            const currentX = event.touches ? event.touches[0].clientX : event.clientX;
+            const deltaX = currentX - startX;
+            const card = document.getElementById(`card-${currentIndex}`);
+
+            if (card) {
+                card.style.transform = `translateX(${deltaX}px) rotate(${(deltaX / 10)}deg)`;
+            }
+            if(currentX < width/5){
+                isDragging=false;
+                card.style.transform = `translateX(40px) rotate(4}deg)`;
+                card.remove;
+                card.style.visibility='hidden';
+                dislike();
+                currentIndex += 1;
+            }
+        }
+    }
+
+    function handleEnd() {
+        if (isDragging) {
+            const card = document.getElementById(`card-${currentIndex}`);
+
+            if (card) {
+                card.style.transform = "translateX(0) rotate(0)";
+            }
+
+            isDragging = false;
+        }
+    }
+
+</script>
+<style>
+    body {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    }
+
+    .chart-container {
+    height: 70vh;
+    aspect-ratio: 2 / 3;
+    perspective: 1000px;
+    perspective-origin: center 50%;
+    transform-style: preserve-3d;
+    position: relative;
+    }
+
+    .card {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    overflow: hidden;
+    transform: translateZ(calc(-30px * var(--i))) translateY(calc(-20px * var(--i))) rotate(calc(-4deg * var(--i)));
+    filter: drop-shadow(2px 2px 20px rgba(0, 0, 0, 0.5));
+    cursor: pointer;
+    user-select: none;
+    transition: transform 0.5s;
+    }
+
+    .card img {
+    border-radius: 20px;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: 50% 50%;
+    }
+    
+</style>
+
+<body>
+    <h1>Tinder des plats</h1>
+    
+<Button shape="pill" color="red" size="lg" on:click={dislike}>
+    <Icon name="x" color="dark"/>
+</Button>
+
+    <div role="tablist" class="chart-container"
+    bind:clientWidth={width}>
+    {#each plats as plat,i} 
+        <button 
+        bind:this={plat[i]}
+        id={`card-${i}`}
+        on:mousedown={handleStart}
+        on:touchstart={handleStart}
+        class="card" style="--i:{i}">
+           {plat}
+            <img src={images[i]} alt="" draggable="false"/>
+        </button>
+    {/each}
+    </div>
+    
+
+
+<Button shape="pill" color="green" size="lg" on:click={like}>
+    <Icon name="heart" color="dark"/>
+</Button>
+</body>
+
+<p>{bool}</p>
+
+<svelte:window 
+on:mousemove={handleMove}
+on:touchmove={handleMove}
+on:mouseup={handleEnd}
+on:touchend={handleEnd} />
+
