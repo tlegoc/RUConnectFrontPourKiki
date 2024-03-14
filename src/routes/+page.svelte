2f4acc2dfb549579b0255d1ef4ec31b6f4c29947
kmarcelin <kilian.marcelin@etudiant.univ-rennes1.fr>
2024-03-14 11:13:26 +0100

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -52,14 +52,26 @@
 
 	let restos = ['Astrolabe', 'INSA', 'Etoile'];
 	let show = false;
+	let show2 = false;
 	let actualRU = 'Astrolabe';
 	function toggleModal() {
 		show = !show;
 	}
 
+	function toggleModalCity() {
+		show2 = !show2;
+	}
+
 	function changeRU(nomRU) {
 		actualRU = nomRU;
 		toggleModal();
+	}
+
+	function changeCity(cityName) {
+		city = cityName;
+		crous = ville_crous[city];
+		getRUs();
+		toggleModalCity();
 	}
 
 	let connectedValue = true;
@@ -73,11 +85,317 @@
 			goto("/login");
 		}
 	}
-	let ip = "";
-	let city = "strasbourg";
-	onMount(() => {
 
-		fetch("https://api.ipify.org?format=json")
+	let villes = [
+		"Paris",
+		"Marseille",
+		"Lyon",
+		"Toulouse",
+		"Nice",
+		"Nantes",
+		"Strasbourg",
+		"Montpellier",
+		"Bordeaux",
+		"Lille",
+		"Rennes",
+		"Reims",
+		"Le Havre",
+		"Cergy",
+		"Saint-Étienne",
+		"Toulon",
+		"Angers",
+		"Grenoble",
+		"Dijon",
+		"Nîmes",
+		"Villeurbanne",
+		"Saint-Denis",
+		"Le Mans",
+		"Aix-en-Provence",
+		"Clermont-Ferrand",
+		"Brest",
+		"Limoges",
+		"Tours",
+		"Amiens",
+		"Metz"
+	];
+	let ville_crous = {
+		"Paris": "paris",
+		"Marseille": "aix.marseille",
+		"Lyon": "lyon",
+		"Toulouse": "toulouse",
+		"Nice": "nice",
+		"Nantes": "nantes",
+		"Montpellier": "montpellier",
+		"Strasbourg": "strasbourg",
+		"Bordeaux": "bordeaux",
+		"Lille": "lille",
+		"Rennes": "rennes",
+		"Reims": "reims",
+		"Le Havre": "normandie",
+		"Saint-Étienne": "lyon",
+		"Toulon": "nice",
+		"Grenoble": "grenoble",
+		"Dijon": "bfc",
+		"Angers": "nantes",
+		"Nîmes": "montpellier",
+		"Villeurbanne": "lyon",
+		"Saint-Denis": "reunion",
+		"Le Mans": "nantes",
+		"Aix-en-Provence": "aix.marseille",
+		"Clermont-Ferrand": "clermont.ferrand",
+		"Brest": "rennes",
+		"Tours": "orleans.tours",
+		"Limoges": "limoges",
+		"Amiens": "amiens",
+		"Annecy": "grenoble",
+		"Perpignan": "montpellier",
+		"Besançon": "bfc",
+		"Metz": "nancy.metz",
+		"Orléans": "orleans.tours",
+		"Rouen": "normandie",
+		"Mulhouse": "strasbourg",
+		"Caen": "normandie",
+		"Nanterre": "paris",
+		"Nancy": "nancy.metz",
+		"Argenteuil": "paris",
+		"Montreuil": "paris",
+		"Roubaix": "lille",
+		"Tourcoing": "lille",
+		"Fort-de-France": "antilles.guyane",
+		"Avignon": "aix.marseille",
+		"Créteil": "creteil",
+		"Poitiers": "poitiers",
+		"Courbevoie": "paris",
+		"Versailles": "versailles",
+		"Colombes": "paris",
+		"La Rochelle": "poitiers",
+		"Antibes": "nice",
+		"Saint-Maur-des-Fossés": "paris",
+		"Calais": "lille",
+		"Béziers": "montpellier",
+		"Bourg-en-Bresse": "lyon",
+		"Issy-les-Moulineaux": "paris",
+		"Levallois-Perret": "paris",
+		"Valence": "grenoble",
+		"Drancy": "paris",
+		"Quimper": "rennes",
+		"Troyes": "reims",
+		"Neuilly-sur-Seine": "paris",
+		"Noisy-le-Grand": "paris",
+		"Antony": "paris",
+		"Vénissieux": "lyon",
+		"Cergy": "paris",
+		"Clichy": "paris",
+		"Niort": "poitiers",
+		"Pessac": "bordeaux",
+		"Chambéry": "grenoble",
+		"Montauban": "toulouse",
+		"Sarcelles": "paris",
+		"Fontenay-sous-Bois": "paris",
+		"Épinay-sur-Seine": "paris",
+		"Évry": "paris",
+		"Chelles": "paris",
+		"Meaux": "paris",
+		"Villejuif": "paris",
+		"Sevran": "paris",
+		"Laval": "nantes",
+		"Chalon-sur-Saône": "bfc",
+		"La Seyne-sur-Mer": "nice",
+		"Le Blanc-Mesnil": "paris",
+		"Maisons-Alfort": "paris",
+		"Pantin": "paris",
+		"Charleville-Mézières": "reims",
+		"Beauvais": "amiens",
+		"Bayonne": "bordeaux",
+		"Cholet": "nantes",
+		"Ivry-sur-Seine": "paris",
+		"Châteauroux": "orleans.tours",
+		"Cannes": "nice",
+		"La Roche-sur-Yon": "nantes",
+		"Hyères": "nice",
+		"Évreux": "normandie",
+		"Châtellerault": "poitiers",
+		"Meudon": "paris",
+		"Saint-Quentin": "amiens",
+		"Albi": "toulouse",
+		"Le Cannet": "nice",
+		"Douai": "lille",
+		"Noisy-le-Sec": "paris",
+		"Tarbes": "toulouse",
+		"Salon-de-Provence": "aix.marseille",
+		"Gagny": "paris",
+		"Villepinte": "paris",
+		"Suresnes": "paris",
+		"Échirolles": "grenoble",
+		"Talence": "bordeaux",
+		"Livry-Gargan": "paris",
+		"Saint-Herblain": "nantes",
+		"Saint-Martin-d'Hères": "grenoble",
+		"Saint-Priest": "lyon",
+		"Martigues": "aix.marseille",
+		"Aubagne": "aix.marseille",
+		"Belfort": "bfc",
+		"Corbeil-Essonnes": "paris",
+		"Brive-la-Gaillarde": "limoges",
+		"Saint-Ouen": "paris",
+		"Montrouge": "paris",
+		"Sainte-Geneviève-des-Bois": "paris",
+		"Massy": "paris",
+		"Bourgoin-Jallieu": "lyon",
+		"Vincennes": "paris",
+		"Villefranche-sur-Saône": "lyon",
+		"La Courneuve": "paris",
+		"Alfortville": "paris",
+		"Sète": "montpellier",
+		"Gap": "aix.marseille",
+		"Bagneux": "paris",
+		"Draguignan": "nice",
+		"Arras": "lille",
+		"Saint-Laurent-du-Var": "nice",
+		"Alès": "montpellier",
+		"Saint-Germain-en-Laye": "paris",
+		"Villeneuve-Saint-Georges": "paris",
+		"La Ciotat": "aix.marseille",
+		"Angoulême": "limoges",
+		"Gennevilliers": "paris",
+		"Saint-Malo": "rennes",
+		"Cachan": "paris",
+		"Montigny-le-Bretonneux": "paris",
+		"Aix-les-Bains": "grenoble",
+		"Carcassonne": "montpellier",
+		"Vitrolles": "aix.marseille",
+		"Vaulx-en-Velin": "lyon",
+		"Bastia": "corse",
+		"Thionville": "nancy.metz",
+		"Cavaillon": "aix.marseille",
+		"Cambrai": "lille",
+		"La Garde": "nice",
+		"Saint-Raphaël": "nice",
+		"Sainte-Foy-lès-Lyon": "lyon",
+		"Bourg-lès-Valence": "grenoble",
+		"Franconville": "paris",
+		"Agen": "bordeaux",
+		"Garges-lès-Gonesse": "paris",
+		"Tremblay-en-France": "paris",
+		"Liévin": "lille",
+		"Saint-Brieuc": "rennes",
+		"Le Chesnay": "paris",
+		"Saint-Leu": "reunion",
+		"Ermont": "paris",
+		"Saint-Chamond": "lyon",
+		"Athis-Mons": "paris",
+		"Roanne": "lyon",
+		"Châlons-en-Champagne": "reims",
+		"Chatou": "paris",
+		"Puteaux": "paris",
+		"Savigny-sur-Orge": "paris",
+		"Sainte-Marie": "reunion",
+		"Haguenau": "strasbourg",
+		"Montluçon": "limoges",
+		"Conflans-Sainte-Honorine": "paris",
+		"Goussainville": "paris",
+		"Saint-Dié-des-Vosges": "nancy.metz",
+		"Saint-Cloud": "paris",
+		"Palaiseau": "paris",
+		"Lens": "lille",
+		"Villeneuve-la-Garenne": "paris",
+		"Villenave-d'Ornon": "bordeaux",
+		"Montbéliard": "bfc",
+		"Saint-Louis": "strasbourg",
+		"Villeparisis": "paris",
+		"L'Haÿ-les-Roses": "paris",
+		"Châtenay-Malabry": "paris",
+		"Saint-Médard-en-Jalles": "bordeaux",
+		"Guyancourt": "paris",
+		"Yerres": "paris",
+		"Houilles": "paris",
+		"Eaubonne": "paris",
+		"Compiègne": "amiens",
+		"Charenton-le-Pont": "paris",
+		"Menton": "nice",
+		"Villemomble": "paris",
+		"Les Ulis": "paris",
+		"Sotteville-lès-Rouen": "normandie",
+		"La Teste-de-Buch": "bordeaux",
+		"Vierzon": "orleans.tours",
+		"Montfermeil": "paris",
+		"Champigny-sur-Marne": "paris",
+		"Plaisir": "paris",
+		"Les Mureaux": "paris",
+		"Bagnolet": "paris",
+		"Carpentras": "aix.marseille",
+		"Vienne": "lyon",
+		"Schiltigheim": "strasbourg",
+		"Saint-Étienne-du-Rouvray": "normandie",
+		"Saint-Ouen-l'Aumône": "paris",
+		"Périgueux": "bordeaux",
+		"Bezons": "paris",
+		"Saint-Michel-sur-Orge": "paris",
+		"Laon": "amiens",
+		"Saumur": "nantes",
+		"Aurillac": "limoges",
+		"Montigny-lès-Metz": "nancy.metz",
+		"Coulommiers": "paris",
+		"Élancourt": "paris",
+		"La Possession": "reunion",
+		"Sannois": "paris",
+		"Saint-Dizier": "reims",
+		"Champs-sur-Marne": "paris",
+		"Creil": "paris",
+		"Saint-Gratien": "paris",
+		"La Garenne-Colombes": "paris",
+		"Dreux": "paris",
+		"Clichy-sous-Bois": "paris",
+		"Châtillon": "paris",
+		"Le Plessis-Robinson": "paris",
+		"Les Lilas": "paris",
+		"Lambersart": "lille",
+		"La Chapelle-sur-Erdre": "nantes",
+		"Pierrefitte-sur-Seine": "paris",
+		"Villiers-le-Bel": "paris",
+		"Saint-Avertin": "orleans.tours",
+		"Sartrouville": "paris",
+		"Plaisance-du-Touch": "toulouse",
+		"Ozoir-la-Ferrière": "paris",
+		"Coudekerque-Branche": "lille",
+		"Malakoff": "paris",
+		"Grigny": "paris",
+		"Épernay": "reims",
+		"Gif-sur-Yvette": "paris",
+		"Vigneux-sur-Seine": "paris",
+		"Rillieux-la-Pape": "lyon",
+		"Combs-la-Ville": "paris",
+		"Lunel": "montpellier",
+		"Auch": "toulouse",
+		"Oullins": "lyon",
+		"L'Isle-sur-la-Sorgue": "aix.marseille",
+		"Oyonnax": "bfc",
+		"Grasse": "nice",
+		"Thiais": "paris"};
+
+	let ip = "";
+	let city = "Strasbourg";
+	let crous = "strasbourg";
+
+	function getRUs(){
+		const queryParams = new URLSearchParams({ get_ids: true });
+		fetch(`https://qx68e2c3ei.execute-api.eu-west-1.amazonaws.com/prod/datagouv/`+crous+`?${queryParams}`)
+			.then(res => res.text()).then(res => {
+				let test = res;
+				var domParser = new DOMParser();
+				var doc = domParser.parseFromString(test, 'text/html');
+				let balises = doc.getElementsByTagName("resto");
+				restos = [];
+				for (let i = 0; i < balises.length; i++) {
+					restos.push(balises[i].getAttribute("title"));
+				}
+
+		})
+	}
+
+	onMount(async () => {
+
+		await fetch("https://api.ipify.org?format=json")
 				.then(response => response.json())
 				.then(data => {
 					ip = data.ip;
@@ -86,20 +404,34 @@
 				});
 
 
-		fetch("http://ip-api.com/json/"+ip+"?lang=fr&fields=city")
+		await fetch("http://ip-api.com/json/"+ip+"?lang=fr&fields=city")
 				.then(response => response.json())
 				.then(data => {
 					city = data.city;
+					crous = ville_crous[city];
+					console.log(city);
+					console.log(crous);
 				}).catch(error => {
 			console.log(error);
 		});
+
+		getRUs();
 	});
+
+
 
 
 </script>
 
 <main>
 	<El col class="XS">
+		<span class="flex center">
+			<h1 style="margin-right: 5px; margin-top: 10px">{city}</h1>
+			<Button size="md"><Icon name="replace" on:click={() =>(toggleModalCity())} /></Button>
+			<Popover trigger="hover" placement="right">
+			<PopoverBody>Changer de Ville</PopoverBody>
+</Popover>
+		</span>
 		<span class="flex center"><h1 style="margin-right: 5px; margin-top: 10px">{actualRU}</h1>
 		<Button size="md"><Icon name="replace" on:click={() =>(toggleModal())} /></Button>
 			<Popover trigger="hover" placement="right">
@@ -182,7 +514,7 @@
 
 
 
-	<Modal size="lg" autoClose backdrop={false} placement="center" bind:show>
+	<Modal size="lg" backdrop={false} placement="center" bind:show={show}>
 		<ModalHeader>
 			<ModalTitle style="width: 100%">
 				<p style="text-align: center">Choisissez votre RU</p>
@@ -192,6 +524,22 @@
 			{#each restos as ru}
 				<Button color="info" on:click={() =>(changeRU(ru))} style="margin-bottom: 5px">
 					{ru}
+				</Button>
+				<br>
+			{/each}
+		</ModalBody>
+	</Modal>
+
+	<Modal size="lg" scrollable backdrop={false} placement="center" bind:show={show2}>
+		<ModalHeader>
+			<ModalTitle style="width: 100%">
+				<p style="text-align: center">Choisissez votre Ville</p>
+			</ModalTitle>
+		</ModalHeader>
+		<ModalBody class="center">
+			{#each villes as city}
+				<Button color="info" on:click={() =>(changeCity(city))} style="margin-bottom: 5px">
+					{city}
 				</Button>
 				<br>
 			{/each}
